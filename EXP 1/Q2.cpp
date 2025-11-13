667f51f2b4e425ed937686f1dd01b4e1c65c61fa
anushkamohite25 <anushka.mohite@mitwpu.edu.in>
2025-11-13 23:15:33 +0530

    Update Q2.cpp

--- EXP 1/Q2.cpp
+++ EXP 1/Q2.cpp
@@ -7,30 +7,26 @@ private:
     int b_pages;
     double b_price;
     string b_name;
-
 public:
     void accept() {
         cout << "Enter Book Name: ";
-        cin.ignore(); // Clear newline from buffer
+        cin.ignore(); 
         getline(cin, b_name);
         cout << "Enter price of Book: ";
         cin >> b_price;
         cout << "Enter no of pages: ";
         cin >> b_pages;
     }
-
     void display() {
         cout << "\n--- Book details ---" << endl;
         cout << "Name of Book: " << b_name << endl;
         cout << "Price of Book: " << b_price << endl;
         cout << "No of pages: " << b_pages << endl;
     }
-
     double getPrice() const {
         return b_price;
     }
 };
-
 int main() {
     Book b1, b2;
     b1.accept();
@@ -41,6 +37,5 @@ int main() {
     } else {
         b2.display();
     }
-
     return 0;
 }
